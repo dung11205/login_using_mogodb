0b9d404bd59d8b0a166a9798589cbc16352c9cba
dung11205 <vandunght00000@gamil.com>
2025-03-29 12:25:10 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = reruire("path");
+const bcrypt = require('bcrypt');
+
+
+const app = require();
+ 
+
+// use ejs as the view engine
+app.set('view engine', 'ejs');
+
+
+
+
+const port = 5000;
+app.listen(port, ()=>{
+    console.log(`sever running on port: ${port}`);
+})
