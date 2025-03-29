84774fc6b205f01863a74e12e81d750f6b258ba4
dung11205 <vandunght00000@gamil.com>
2025-03-29 12:30:00 +0700

    Update code

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ const app = require();
 // use ejs as the view engine
 app.set('view engine', 'ejs');
 
+app.get("/" , (req,res)=>{
+    res.render("login");
+})
+
+app.get("/signup", (req,res)=>{
+    res.render("signup");
+})
 
 
 
